764d06f05f2edc7256d0b6fd00101107ad8ed1ec
Samuel Mello <mello.developer797@gmail.com>
2024-10-02 16:30:37 -0300

    Commit para segurança de que tudo está no github

--- src/main/java/com/spring_openapi/application/config/SwaggerConfig.java
+++ src/main/java/com/spring_openapi/application/config/SwaggerConfig.java
@@ -13,7 +13,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenApi() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("API de pessoas")
+                        .title("API de Pessoas")
                         .version("1.5")
                         .description("API para gerenciamento de pessoas")
                         .contact(new Contact()
